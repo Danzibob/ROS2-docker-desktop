cda1007a3d1a6cc317214228ce163c5a6b726f7a
Danny Roberts <danzibob@gmail.com>
2023-10-25 12:02:49 +0100

    Update for Docker Compose v2.x

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 # Check if the ROS-desktop container is already running; start if not
 container_id=$(docker ps -q -f name=ros-desktop)
 if [ -z "$container_id" ]; then
-  echo "No ros-desktop container is running. Starting a new one."
+  echo "No ros-desktop container is running. Starting a new one..."
   docker-compose up -d
   container_id=$(docker ps -l -q)
   
@@ -12,11 +12,20 @@ if [ -z "$container_id" ]; then
 
   # Open an interactive terminal in the container
   ./new_terminal.sh
+  # Continues once the interactive session ends
 
-  # Once the interactive session ends, stop container & revoke xhost perms
+  echo "Main terminal session closed, stopping and cleaning up container..."
+  
+  # stop container & revoke xhost perms
   docker stop -t 0 $container_id
   xhost -local:$container_id
+  
+  echo "Done!"
+  
 else
+  # Open a terminal session in the existing container
   echo "A ros-desktop container is already running with ID: $container_id"
-  echo "To start another ros-desktop container, please stop that one first"
+  echo "Starting an interactive terminal session..."
+  ./new_terminal.sh
+  printf "\nContainer was closed! Exiting..."
 fi
